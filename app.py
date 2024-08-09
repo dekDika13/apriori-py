16b95d18b202112232624aca856ccd4c32f6be56
dekDika13 <kadekdikaramasuryadi@gmail.com>
2024-08-09 14:05:22 +0800

    change_name_database

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'retail'
+app.config['MYSQL_DB'] = 'rule'
 
 # Konfigurasi untuk pengunggahan file
 app.config['UPLOAD_FOLDER'] = 'uploads'  # Folder untuk menyimpan file yang diunggah
